5f1bf84d89a72de6af655ce8a7bebc904c57fe30
dickynovanto1103 <dickynovanto1103@gmail.com>
2020-04-03 00:31:24 +0800

    Add experiment calling SG Bus API
    Hide the accountkey in env variable

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -13,8 +15,63 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	log.Println("byte", byte, "written")
-
+	//
 	//call the API here
+	accountKey := os.Getenv("accountkey")
+	callBusAPI(accountKey)
+	//callBusStopAPI(accountKey)
+}
+
+func callBusAPI(accountKey string) {
+	url := "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=83139"
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil || req == nil {
+		log.Println("err: ", err)
+		return
+	}
+	req.Header.Add("AccountKey", accountKey)
+	req.Header.Add("accept", "application/json")
+	res, err := client.Do(req)
+	//res, err := http.Post(url, "application/json", bytes.NewReader(jsonBody))
+	if err != nil {
+		log.Println("error http get, err: ", err)
+		return
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Println("fail to read all, err: ", err)
+		return
+	}
+	log.Println("body:", string(body))
+	defer res.Body.Close()
+	fmt.Println("res:", res)
+}
+
+func callBusStopAPI(accountKey string) {
+	url := "http://datamall2.mytransport.sg/ltaodataservice/BusStops"
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil || req == nil {
+		log.Println("err: ", err)
+		return
+	}
+	req.Header.Add("AccountKey", accountKey)
+	req.Header.Add("accept", "application/json")
+	res, err := client.Do(req)
+	if err != nil {
+		log.Println("error http get, err:", err)
+		return
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Println("error read all, err:", err)
+		return
+	}
+	log.Println("body:", string(body))
 }
 
 func main() {
