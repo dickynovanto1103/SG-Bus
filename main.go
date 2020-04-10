28d9b665832c9ab8ade709f1b391addc79d9d576
dickynovanto1103 <dickynovanto1103@gmail.com>
2020-04-10 23:32:57 +0800

    Add header in response to allow CORS
    Add Header with key Access-Control-Allow-Origin, and allowing all
    origins to request to this API

--- main.go
+++ main.go
@@ -9,41 +9,49 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	byte, err := w.Write([]byte("halo"))
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	//byte, err := w.Write([]byte("halo"))
+	//if err != nil {
+	//	fmt.Println("error: ", err)
+	//	return
+	//}
+	//log.Println("byte", byte, "written")
+	//
+	//call the API here
+	accountKey := os.Getenv("accountkey")
+	res := callBusAPI("http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=19091", accountKey)
+	//callBusAPI("http://datamall2.mytransport.sg/ltaodataservice/BusStops", accountKey)
+	bytes, err := w.Write(res)
 	if err != nil {
 		fmt.Println("error: ", err)
 		return
 	}
-	log.Println("byte", byte, "written")
-	//
-	//call the API here
-	accountKey := os.Getenv("accountkey")
-	callBusAPI("http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=19091", accountKey)
-	//callBusAPI("http://datamall2.mytransport.sg/ltaodataservice/BusStops", accountKey)
+	log.Println(bytes, "bytes written")
 }
 
-func callBusAPI(url, accountKey string) {
+func callBusAPI(url, accountKey string) []byte {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil || req == nil {
 		log.Println("err: ", err)
-		return
+		return []byte{}
 	}
 	req.Header.Add("AccountKey", accountKey)
 	req.Header.Add("accept", "application/json")
 	res, err := client.Do(req)
 	if err != nil {
 		log.Println("error http get, err: ", err)
-		return
+		return []byte{}
 	}
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		log.Println("fail to read all, err: ", err)
-		return
+		return []byte{}
 	}
 	log.Println("body:", string(body))
 	defer res.Body.Close()
 	fmt.Println("res:", res)
+	return body
 }
 
 func main() {
